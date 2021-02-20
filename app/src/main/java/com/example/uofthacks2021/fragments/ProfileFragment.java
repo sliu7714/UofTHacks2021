d21f734484d4e24b58e248c8ae4bee2396179987
sliu7714 <siyuanliu3@gmail.com>
2021-02-20 11:13:03 -0700

    Started profile fragment

--- app/src/main/java/com/example/uofthacks2021/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/uofthacks2021/fragments/ProfileFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,12 +18,24 @@ import com.example.uofthacks2021.R;
 
 public class ProfileFragment extends Fragment {
 
+    CheckBox cb1;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
+
+        cb1 = (CheckBox)root.findViewById(R.id.tag1);
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(!isChecked){
+                    cb1.setVisibility(View.GONE);
+                }
+            }
+        });
         return root;
     }
 }
